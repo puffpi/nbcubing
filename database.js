6f7ee06c19d4eb372baf38beed2d19a10593992f
puffpi <guochang416e2d@gmail.com>
2026-09-03 17:05:07 +0800

    Add files via upload

---
+++ database.js
@@ -0,0 +1,39 @@
+const rosterIds = [
+    '2024GUOC01', '2024FENG08', '2023WANY03', '2018WANH02',
+    '2016WUJI01', '2018HUAN49', '2019ZHOU88', '2025CHEJ08',
+    '2026QIAN04', '2025HUAN11', '2025JIAN23', '2025WANB01',
+    '2018ZHAX04', '2018SUNK01', '2024WANG52', '2025QING03',
+    '2023CHEJ12', '2023CHEN30', '2018ZHAJ16', '2016HEZH01',
+    '2018WANZ50', '2011HUAN10', '2018SHAO02', '2017DONG13',
+    '2015CAIT01', '2017YANY06', '2021HUAN17', '2015ZHUJ05',
+    '2018ZHUH05', '2017ZHUY03', '2014CHEN33', '2018WANH29',
+    '2021WENZ02', '2026DING03', '2017PANL01', '2014LITI01',
+    '2009FENG07', '2011YANX02', '2016ZHOU21', '2016ZHOU16',
+    '2015LINX02', '2018GUOZ07', '2018ZHAO61', '2018WANG12',
+    '2018GOUS01', '2018XIEN01', '2018XUSH07', '2018QIZI01',
+    '2018LIHA15', '2018ZHAO59', '2018SHIY09', '2018LINY10',
+    '2018ZOUZ02', '2018ZHAO62', '2017ZHOU10', '2015LIJI05',
+    '2011FANG03', '2019CHEQ01', '2026JIAN17', '2016HEJI02',
+    '2015FUYU01', '2026ZHEN08', '2025CHEN46', '2025SHEN26',
+    '2025QIHA02', '2025HUAY04', '2024XULI01', '2025LIZH05',
+    '2025WANG45', '2019CHEY56', '2019CHEN92', '2014ZHUC01',
+    '2025LOUJ01', '2019GUAN18', '2024GUAN02', '2025WUBO02',
+    '2024LUOK01', '2025ZHEN27', '2025SHEN27', '2025LICH03',
+    '2025ZHAZ11', '2026ZHAN79', '2025TANW01'
+];
+
+const eventDict = [
+    { id: '333', name: '三阶' }, { id: '222', name: '二阶' },
+    { id: '444', name: '四阶' }, { id: '555', name: '五阶' },
+    { id: '666', name: '六阶' }, { id: '777', name: '七阶' },
+    { id: '333bf', name: '三盲' }, { id: '333fm', name: '最少步' },
+    { id: '333oh', name: '单手' }, { id: 'clock', name: '魔表' },
+    { id: 'minx', name: '五魔方' }, { id: 'pyram', name: '金字塔' },
+    { id: 'skewb', name: '斜转' }, { id: 'sq1', name: 'SQ1' },
+    { id: '444bf', name: '四盲' }, { id: '555bf', name: '五盲' },
+    { id: '333mbf', name: '多盲' },
+    { id: '333ft', name: '脚拧' },
+    { id: 'magic', name: '八板' },
+    { id: 'mmagic', name: '十二板' },
+    { id: 'mbf', name: '旧版多盲' }
+];
